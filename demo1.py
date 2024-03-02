9cd51fcb53ef4f2d7196e6ad7831d22c998803c3
MeryemLM <121564877+MeryemLM@users.noreply.github.com>
2024-03-02 20:37:35 +0100

    Update demo1.py

--- demo1.py
+++ demo1.py
@@ -30,7 +30,7 @@ def analyze_sentiment_voice(text):
     return sentiment_label
 
 # Set up OpenAI API key
-openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
+openai.api_key = st.sidebar.text_input('OpenAI API Key', type='password')
 
 
 def analyze_emotion(text):
