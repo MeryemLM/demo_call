661e8ba278b0ae567147f6138e781e86a4388a8b
MeryemLM <121564877+MeryemLM@users.noreply.github.com>
2024-03-03 18:56:05 +0100

    Update demo1.py

--- demo1.py
+++ demo1.py
@@ -50,12 +50,19 @@ def analyze_emotion(text):
    except Exception as e:
        print(f"Erreur lors de l'analyse de l'émotion : {e}")
        return None
+with st.sidebar:
+   display_image("./logo2.jpg", width=200)
+
+    #option = st.sidebar.selectbox("Current option", ["Upload audio file"])
+ 
+   for _ in range(25):
+       st.sidebar.text("") 
 
 # Streamlit app
 def main():
      
     # Titre de l'application
-    st.markdown("<h1 style='text-align:center; color: white;'>Transcription audio et analyse émotionnelle</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align:center; color: #B01817;'>Transcription audio et analyse émotionnelle</h1>", unsafe_allow_html=True)
     #st.title("Analyse de la transcription audio")
 
     option = st.sidebar.selectbox("Choose an option", ["Upload audio file", "Audio file path"])
